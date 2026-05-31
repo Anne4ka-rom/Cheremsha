d44acab94111f76c30c657fe1e6e1c5b99c74636
Golubkov Nikita <golubkoffnickita0@gmail.com>
2026-05-31 03:25:51 +0300

    Первый вариант FastApi приложения

---
+++ fastapi_app/models.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class RequestStatus(BaseModel):
+    status: int
+    message: str
+
+class PredictRequest(BaseModel):
+    info: dict
+
+class PredictResponse(BaseModel):
+    info: dict
+    loan_status: int
+
+class DatasetResponse(BaseModel):
+    dataset: bytes
+    roc_auc: Optional[float] = Field(default=None)
